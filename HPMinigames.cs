35d02584532b48fb73746beef74a04b23770e253
Leonhard Seidel <leonhardseidel@gmx.de>
2022-04-17 18:48:30 +0200

    Moved freecam to a standalone mod

--- HPMinigames.cs
+++ HPMinigames.cs
@@ -5,7 +5,6 @@ using UnityEngine.InputSystem;
 using EekAddOns;
 using UnhollowerRuntimeLib;
 using EekCharacterEngine.Interaction;
-using HPmdk;
 using HouseParty;
 using EekCharacterEngine;
 using EekEvents;
@@ -20,79 +19,35 @@ namespace HPInvader
         public static bool inMainMenu = false;
         public static bool inGameMain = false;
         private bool inMinigame = false;
-        private Freecam freecam;
         private HousePartyPlayerCharacter player = null;
         public Rect windowRect = new Rect(20, 20, 400, 200);
-        private readonly float speed = 2f;
         private readonly float sideSpeed = 0.5f;
         private readonly float forwardSpeed = 0.05f;
+        private readonly Vector3 startPos = new Vector3(48.7f, -28.9f, 2);
         AmyCharacter amy;
         float amyTimer = 1;
         Camera camera;
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            freecam = new Freecam(Object.FindObjectOfType<Camera>());
             camera = Object.FindObjectOfType<Camera>();
             InitializeGameObjects();
         }
 
         public override void OnUpdate()
         {
-            //toggle freecam with alt+f
-            if (Keyboard.current[Key.F].wasPressedThisFrame && Keyboard.current[Key.LeftAlt].isPressed)
-            {
-                if (freecam.Enabled())
-                {
-                    freecam.SetDisabled();
-                    freecam.SetRotationDisabled();
-                    if (inGameMain)
-                    {
-                        player.Incapacitated = false;
-                        player.IsImmobile = false;
-                        Camera temp_camera = Object.FindObjectOfType<Camera>();
-                        if ((temp_camera.transform.position - player.Head.position + new Vector3(0, 0.1f, 0) + Vector3.Scale(player.Head.forward, new Vector3(0.2f, 0.2f, 0.2f))).magnitude >= 0.3)
-                        {
-                            for (int i = 0; i < 10; i++)
-                            {
-                                temp_camera.transform.position = player.Head.position + new Vector3(0, 0.1f, 0) + Vector3.Scale(player.Head.forward, new Vector3(0.2f, 0.2f, 0.2f));
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (inGameMain)
-                    {
-                        player.Incapacitated = true;
-                        player.IsImmobile = true;
-                    }
-                    else
-                    {
-                        freecam.SetRotationEnabled();
-                    }
-                    freecam.SetEnabled();
-                }
-
-            }
-            else if (Keyboard.current[Key.U].wasPressedThisFrame && Keyboard.current[Key.LeftAlt].isPressed)
+            if (Keyboard.current[Key.U].wasPressedThisFrame && Keyboard.current[Key.LeftAlt].isPressed)
             {
                 //toggle in minigame
                 inMinigame = !inMinigame;
                 if (inMinigame)
                 {
-                    MinigameInit();
+                    FaceInvaderInit();
                 }
                 else
                 {
-                    ResetMinigame();
+                    ResetFaceInvader();
                 }
-            }
-
-            //run freecam
-            if (freecam.Enabled())
-            {
-                freecam.Update();
             }
             if (inGameMain)
             {
@@ -117,12 +72,12 @@ namespace HPInvader
             }
         }
 
-        private void ResetMinigame()
+        private void ResetFaceInvader()
         {
             if (amy != null)
             {
                 amy.IsImmobile = false;
-                //amy._activePositionOverride = false;
+                amy._activePositionOverride = false;
                 amy.Mecanim.OGBKIDBHJDB(); //end pose
                 amy.Clothes.ACMHNFDKNLL(EHFOEJADICB.Shoes, true, 0);
                 amy.Clothes.ACMHNFDKNLL(EHFOEJADICB.Bottom, true, 0);
@@ -131,24 +86,22 @@ namespace HPInvader
             }
             player.Incapacitated = false;
             player.IsImmobile = false;
-            //player._activePositionOverride = false;
+            player._activePositionOverride = false;
             player.Intimacy.LockPenis = false;
-            //player.Intimacy._penisDB.enabled = true;
+            player.Intimacy.CustomPenisScaleMultiplier = 1f;
             player.Clothes.ACMHNFDKNLL(EHFOEJADICB.Shoes, true, 0); //set clothing item
             player.Clothes.ACMHNFDKNLL(EHFOEJADICB.Bottom, true, 0);
             player.Clothes.ACMHNFDKNLL(EHFOEJADICB.Top, true, 0);
-            //GameManager.ILMGIKEOAPM().UnPause();//get current game manager
         }
 
-        private void MinigameInit()
+        private void FaceInvaderInit()
         {
             amy = Object.FindObjectOfType<AmyCharacter>();
             if (amy != null)
             {
                 amy.Mecanim.HPCPOABEPPO(10); //set pose 10, blowjobready
                 amy.eventQueue.Clear();
-                //amy.SetPositionOverride(new Vector3(0.5f, 0.3f, -6));
-                amy.transform.position = new Vector3(0.5f, 0.3f, -6);
+                amy.SetPositionOverride(startPos);
                 amy.IsImmobile = true;
                 amy.Clothes.AMODMPBOFOK(false, 0);
                 amyTimer = amy.Mecanim._animationEventQueueTimer;
@@ -157,37 +110,65 @@ namespace HPInvader
             }
             player.Incapacitated = true;
             player.IsImmobile = true;
-            player.SetPositionOverride(new Vector3(0.5f, 0.3f, -5));//todo add random offset when starting the minigame
-            //player.FPInput.AllowCrouch = false;
-            //player.FPInput.Enabled = false;
-            //player.transform.position = new Vector3(0.5f, 0.3f, -5);
+            player.SetPositionOverride(startPos + new Vector3(Random.RandomRange(-0.5f, 0.5f), Random.RandomRange(-0.5f, 0.5f), Random.RandomRange(-0.5f, 0.5f) + 2));//start position 2 units in front of amy, plus random offset
+            player.FPInput.AllowCrouch = false;
             player.Clothes.AMODMPBOFOK(false, 0); //set all clothing
-            //else do targeting by myselfusing _penisBone and _penisDB (dynamic bone)
+            player.Intimacy.BlowJobLookTarget = amy.gameObject;
+            player.Intimacy.LockPenis = true;
+
+            player.Intimacy.CustomPenisScaleMultiplier = 1.1f;
+
+            Physics.gravity = Vector3.zero;
+
+            //ground layer is 8, walls are 17
+            /*
+            foreach (var coll in Object.FindObjectsOfType<Collider>())
+            {
+                coll.enabled = false;
+            }
+            */
+            //things that did not work:
+            //#############################
+            //player.PuppetMaster.FBPMBCBPMFM(10000);//deactivate pupet for 10000 seconds
+            //player.FPInput.Enabled = false;
             //player.Intimacy._currentPenisAngle = 90;
             //player.Intimacy.iHelper.BOCNBFJCMKI(OGHGPAGLEBD.Masturbating, player);//start sex event, masturbating. works for the dick but i need the hand to not be there...
             //player.Intimacy.FIBHPFAHJDI(1);//release left hand
-            player.Intimacy.BlowJobLookTarget = amy.gameObject;
-            //player.Intimacy.AMCJJPGDNIE(OGHGPAGLEBD.BlowJob, amy, LKLCNNABKHF.Primary); //initiate sex event
-            player.Intimacy.LockPenis = true;
-            player._myColliders.Clear();
-            player.Motion.ApplyGravity = false;
-            player.FPInput.AllowFlying = true;
-            amy.Motion.ApplyGravity = false;
-            //amy.breast
-            //todo disable gravity/floor ik for the player and amy
-            //remove all colliders from the player
+            //player.Intimacy.AMCJJPGDNIE(OGHGPAGLEBD.BlowJob, amy, LKLCNNABKHF.Primary); //initiate sex event (works, but doesnt help)
+            //player.BlendShapes.EmoteList.Clear();//clear list of current emotes
+            //player.BlendShapes.PCHBGDPOPMB(true);//clear all remotes, also important ones
+            //player.BlendShapes.BPNJFBDBHDD(IKHHCHDDNCI.Erection, true, 1);//erection emote
+            //player.BlendShapes.FADECPNLCAE();//set penis erect
 
-            //GameManager.ILMGIKEOAPM().Pause();
+            //amy.BlendShapes.EmoteList.Clear();
+            //#############################
+
+            //amy.breast
+
+            foreach (var item in amy.BlendShapes.EmoteList)
+            {
+                MelonLogger.Msg($"Emote in Amy's EmoteList: id {item.Id} | blendshapeId {item.BlendShapeId} | is full face? {item.FullFace}");
+            }
+            foreach (var item in player.BlendShapes.EmoteList)
+            {
+                MelonLogger.Msg($"Emote in Players's EmoteList: id {item.Id} | blendshapeId {item.BlendShapeId} | is full face? {item.FullFace}");
+            }
+
+            //doesnt work :(
+            amy.BlendShapes.PCHBGDPOPMB(true);//reset blend shapes (important ones true)
+            amy.BlendShapes.PPBOLAJOLMP(true);//reset talking blend shapes (important ones true)
+            amy.BlendShapes.currentTargetImportance = AEOLDKJIEHJ.None;
+            amy.BlendShapes.Target = player.gameObject;
+            amy.BlendShapes.currentTargetImportance = AEOLDKJIEHJ.VeryImportant;
+            amy.BlendShapes._currentImportantEmotes.Clear();
+            amy.BlendShapes._currentImportantEmotes.Add(amy.BlendShapes.EmoteList[8]);
+            amy.BlendShapes.BPNJFBDBHDD(IKHHCHDDNCI.OpenMouth, true, 8); //blendshapes emote (emote, important, emotestr??)
+
         }
 
         private void MinigameFaceInvader()
         {
-            player.Intimacy.FIBHPFAHJDI(1);
-            amy = Object.FindObjectOfType<AmyCharacter>();
-            if (amy != null)
-            {
-                amy.transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
+            amy.transform.rotation = Quaternion.Euler(0, 0, 0);
 
             player.transform.rotation = Quaternion.Euler(0, 180, 0);
             player.Intimacy._penisBone.rotation = Quaternion.Euler(0, 180, 0);//move dick to the right orientation
@@ -197,17 +178,8 @@ namespace HPInvader
             player.Intimacy._penisDB.m_FreezeAxis = DynamicBone.IIJOFOGIFHD.Y; //freeze zhe y axis
             player.Intimacy._penisDB.m_Gravity = Vector3.zero; //remove gravity so it stays up
             player.Intimacy._penisDB.m_Inert = 100;
-            amy.BlendShapes.EmoteList.Clear();
-            amy.BlendShapes.PCHBGDPOPMB(true);//reset blend shapes (important ones true)
-            //player.BlendShapes.EmoteList.Clear();
-            //player.BlendShapes.PCHBGDPOPMB(true);
 
-            amy.BlendShapes.BPNJFBDBHDD(IKHHCHDDNCI.OpenMouth, true, 1); //blendshapes emote (emote, important, emotestr??)
-            //player.BlendShapes.BPNJFBDBHDD(IKHHCHDDNCI.Erection, true, 1);
-
-            player.BlendShapes.FADECPNLCAE();//set penis erect
-
-            camera.transform.position = player.puppetHip.position + player.puppetHip.forward * 0.127f - player.puppetHip.up * 0.08f;
+            camera.transform.position = player.Intimacy._penisBone.position + player.Intimacy._penisBone.up * 0.07f + player.Intimacy._penisBone.forward * 0.02f;
 
             //move player forward with constant time
             player._positionOverride += player.transform.forward * forwardSpeed * Time.deltaTime;
@@ -228,6 +200,8 @@ namespace HPInvader
             {
                 player._positionOverride -= player.transform.up * sideSpeed * Time.deltaTime;
             }
+
+            amy.BlendShapes.BPNJFBDBHDD(IKHHCHDDNCI.OpenMouth, true, 8); //blendshapes emote (emote, important, emotestr??)
         }
 
         public void Test()
